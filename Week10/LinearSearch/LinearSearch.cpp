92f23062e4b6124d7851aad96e0e92c5711eb6ad
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-12-07 09:47:53 +0100

    Finish linear search

--- Week10/LinearSearch/LinearSearch.cpp
+++ Week10/LinearSearch/LinearSearch.cpp
@@ -1,2 +1,22 @@
 /* Implement a program to linearly search through an unsorted array*/
 /* Bonus: Implement a boolean search on a sorted array*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace  std;
+
+bool linearSearch(const int [], const unsigned int, const int);
+
+int main(){
+    int array[6] = {1,5,4,2,6,1};
+    cout << linearSearch(array, 6, 2) << endl;
+    cout << linearSearch(array, 6, 9);
+    return 0;
+}
+
+bool linearSearch(const int array[], const unsigned int size, const int elementToFind){
+    for (unsigned int i = 0; i < size; i++)
+        if (array[i] == elementToFind)
+            return true;
+    return false;
+}
