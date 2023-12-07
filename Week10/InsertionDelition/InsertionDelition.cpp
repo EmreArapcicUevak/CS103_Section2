867afcd23971d4122c4061a875b4dbaa8b999e1c
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-12-07 10:10:54 +0100

    Finish Insertion elements

--- Week10/InsertionDelition/InsertionDelition.cpp
+++ Week10/InsertionDelition/InsertionDelition.cpp
@@ -1,1 +1,27 @@
-/* Make a function that allows for insertion and delition of a specific element [deletion = setting to some default value]*/
+/* Make a function that allows for insertion and delition of a specific element [deletion = setting to
+ * some default value]*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void inserElement(int [], const unsigned int, const unsigned int);
+void deleteElement(int [], const unsigned int, const unsigned int);
+
+int main(){
+
+    return 0;
+}
+void inserElement(int array[], const unsigned int size, const unsigned int index){
+    if (index >= size)
+        cerr << "Invalid Index!";
+
+    cout << "Please enter a value: ";
+    cin >> array[index];
+}
+void deleteElement(int array[], const unsigned int size, const unsigned int index){
+    if (index >= size)
+        cerr << "Invalid Index!";
+
+    array[index] = 0;
+}
